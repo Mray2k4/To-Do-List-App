17f30bcae91f2c50b826de7ee774e4c67b5f12d1
Marque Ray <mray2k4@yahoo.com>
2023-05-08 09:23:12 -0700

    added checkbox and delete button to list

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const input = document.querySelector('#txt');
 const add = document.querySelector('#btn');
 const list = document.querySelector('.container ul');
+const hide = document.querySelector('#delete');
+
+
+
 
 add.addEventListener('click', function(m) {
-    if(input.value != "") {
+      if(input.value != "") {
         m.preventDefault();
         const myList = document.createElement('li');
+        const check = document.createElement('input')
+        const remove = document.createElement('button');
+        check.setAttribute('type', 'checkbox');
+        remove.setAttribute('id', 'delete')
         myList.innerHTML = input.value;
+        remove.innerHTML = 'x'
+        list.appendChild(check);
         list.appendChild(myList);
+        list.appendChild(remove);
+        
     }
+
 })
