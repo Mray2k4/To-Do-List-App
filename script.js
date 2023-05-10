2f7d54ce10dee549d31104947be8452468709b12
Marque Ray <mray2k4@yahoo.com>
2023-05-09 19:48:57 -0700

    added delete button when strikethrough happens

--- script.js
+++ script.js
@@ -8,24 +8,28 @@ add.addEventListener('click', function(m) {
         m.preventDefault();
         const myList = document.createElement('li');
         const check = document.createElement('input');
-        const remove = document.createElement('button');
+        //const remove = document.createElement('button');
         check.setAttribute('type', 'checkbox');
         check.setAttribute('id', 'click');
-        remove.setAttribute('id', 'delete');
+        //remove.setAttribute('id', 'delete');
         myList.innerHTML = input.value;
-        remove.innerHTML = 'x'
+        //remove.innerHTML = 'x'
         list.appendChild(check);
         list.appendChild(myList);
-        list.appendChild(remove);
+        //list.appendChild(remove);
     }
 
     let click = document.getElementById('click');
 click.addEventListener('change', function() {
     let line = document.getElementById('ordered');
+    const remove = document.createElement('button');
+    remove.setAttribute('id', 'delete');
+    remove.innerHTML = 'x';
+    list.appendChild(remove);
     if(click.checked) {
        line.style.setProperty("text-decoration", "line-through");
     } else {
-        line.setAttribute('disabled', 'disabled');
+        line.removeEventListener(click);
     }
 })
     
