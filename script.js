638dd79d1f1abe7536f7c545f8c26a7b73ab71b3
Marque Ray <mray2k4@yahoo.com>
2023-05-09 19:22:41 -0700

    test checkbox click

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
 const input = document.querySelector('#txt');
 const add = document.querySelector('#btn');
-const list = document.querySelector('.container ul');
-const hide = document.querySelector('#delete');
-
-
-
+const list = document.querySelector('.list');
+const close = document.querySelector('#delete');
 
 add.addEventListener('click', function(m) {
       if(input.value != "") {
         m.preventDefault();
         const myList = document.createElement('li');
-        const check = document.createElement('input')
+        const check = document.createElement('input');
         const remove = document.createElement('button');
         check.setAttribute('type', 'checkbox');
-        remove.setAttribute('id', 'delete')
+        check.setAttribute('id', 'click');
+        remove.setAttribute('id', 'delete');
         myList.innerHTML = input.value;
         remove.innerHTML = 'x'
         list.appendChild(check);
         list.appendChild(myList);
         list.appendChild(remove);
-        
     }
 
+    let click = document.getElementById('click');
+click.addEventListener('change', function() {
+    let line = document.getElementById('reg');
+    if(click.checked) {
+       line.removeAttribute('disabled');
+    } else {
+        line.setAttribute('disabled', 'disabled');
+    }
 })
+    
+    input.value = "";
+})
+
+
